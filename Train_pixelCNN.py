26943ba423b0e2d989fefbda19b26119c367e2f2
Alex Pierron <118303067+AP496@users.noreply.github.com>
2023-01-31 16:43:17 +0100

    fixing additional problems

--- Train_pixelCNN.py
+++ Train_pixelCNN.py
@@ -8,6 +8,7 @@ import torchvision
 from torchvision import datasets
 from torchvision.datasets import MNIST
 import torchvision.transforms as transforms
+from torch.autograd import Variable
 
 from pixelCNN import PixelCNN
 
@@ -15,47 +16,104 @@ device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 
 data_train = torch.utils.data.DataLoader(
-    MNIST(
-          '~/mnist_data', train=True, download=True, 
-          transform = transforms.Compose([
-              transforms.ToTensor()
-          ])),
-          batch_size=16,
-          shuffle=True
-          )
+	MNIST(
+		  '~/mnist_data', train=True, download=True, 
+		  transform = transforms.Compose([
+			  transforms.ToTensor()
+		  ])),
+		  batch_size=16,
+		  shuffle=True
+		  )
 
 data_test = torch.utils.data.DataLoader(
-    MNIST(
-          '~/mnist_data', train=False, download=True, 
-          transform = transforms.Compose([
-              transforms.ToTensor()
-          ])),
-          batch_size=16,
-          shuffle=True
-          )
+	MNIST(
+		  '~/mnist_data', train=False, download=True, 
+		  transform = transforms.Compose([
+			  transforms.ToTensor()
+		  ])),
+		  batch_size=16,
+		  shuffle=True
+		  )
 
-def train_loop(dataloader, model, loss_fn, optimizer):
-    size = len(dataloader.dataset)
-    for batch, (X, y) in enumerate(dataloader):
-        # Compute prediction and loss
-        pred = model(X)
-        loss = loss_fn(pred,y)
+""""
+for batch_idx, (X,Y) in enumerate(data_test):
+	  print(batch_idx)
 
-        # Backpropagation (always in three steps)
-        optimizer.zero_grad() # a toujours mettre pour pas accumuler les gradients
-        loss.backward()
-        optimizer.step()
-
-        if batch % 100 == 0:
-            loss, current = loss.item(), batch * len(X)
-            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
-
+for images, labels in data_test:
+	print(labels)
+"""
 model = PixelCNN()
 
 # Hyperparameters
 learning_rate = 0.01
 batch_size = 2**10
-epochs = 3
-loss_fn = torch.nn.Softmax()
+epochs = 2
+loss_fn = nn.CrossEntropyLoss()
 optimizer = torch.optim.RMSprop(model.parameters())
 
+def train_loop(dataloader, model, loss_fn, optimizer):
+	size = len(dataloader.dataset)
+	for batch, (X, y) in enumerate(dataloader):
+		target = Variable(X[:,0,:,:]*255).long()
+		X = X.to(device)
+		target = target.to(device)
+		# Compute prediction and loss
+		pred = model(X)
+		loss = loss_fn(pred,target)
+
+		# Backpropagation (always in three steps)
+		optimizer.zero_grad() # a toujours mettre pour pas accumuler les gradients
+		loss.mean().backward()
+		optimizer.step()
+
+		if batch % 100 == 0:
+			loss, current = loss.item(), batch * len(X)
+			print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+	
+
+
+'''
+loss_overall = []
+for i in range(epochs):
+
+		
+		model.train(True)
+		step = 0
+		loss_= 0
+
+		for images, labels in data_train:
+			
+			target = Variable(images[:,0,:,:]*255).long()
+			images = images.to(device)
+			target = target.to(device)
+			
+			
+
+
+			optimizer.zero_grad()
+
+			output = model(images)
+			loss = loss_fn(output)
+			loss.mean().backward()
+			optimizer.step()
+
+
+			loss_+=loss
+			step+=1
+
+			if(step%100 == 0):
+				print('Epoch:'+str(i)+'\t'+ str(step) +'\t Iterations Complete \t'+'loss: ', loss.item()/1000.0)
+				loss_overall.append(loss_/1000.0)
+				loss_=0
+		print('Epoch: '+str(i)+' Over!')
+
+'''
+
+for t in range(epochs):
+	print(f"Epoch {t+1}\n-------------------------------")
+	train_loop(data_train,model,loss_fn,optimizer)
+print("Done!")
+
+#Saving the network
+PATH = './mnist.pth'
+torch.save(model.state_dict(), PATH)
--- pixelCNN.py
+++ pixelCNN.py
@@ -47,34 +47,36 @@ class PixelCNN(nn.Module):
 		self.device = device
 		#first convolution (cf TABLE 1)
 		self.ConvA = nn.Sequential(
-			MaskedConv2D('A',3, 2*channels, kernel_size=7, bias=False),
+			MaskedConv2D('A',1, 2*self.channels, kernel_size=7, bias=False),
 			nn.ReLU(True)
 		)
+		self.test = nn.Conv2d(2*self.channels,self.channels, kernel_size = 1, bias=False)
+		self.test2 = MaskedConv2D('B',self.channels,self.channels, kernel_size = 3,padding="same", bias=False)
 		#Residual blocks for PixelCNN (figure 5)
 		self.multiple_blocks = nn.Sequential(
-			nn.Conv2d(2*channels,channels, kernel_size = 1, bias=False),
+			nn.Conv2d(2*self.channels, self.channels, kernel_size = 1, bias=False),
 			nn.ReLU(True),
-			MaskedConv2D('B',channels,channels, kernel_size = 3, bias=False),
+			MaskedConv2D('B',self.channels,self.channels, kernel_size = 3,padding='same', bias=False),
 			nn.ReLU(True),
-			nn.Conv2d(channels,2*channels, kernel_size = 1, bias=False),
+			nn.Conv2d(self.channels,2*self.channels, kernel_size = 1, bias=False),
 			nn.ReLU(True)
 		)
 		#finalisation
 		self.end = nn.Sequential(
 			nn.ReLU(True),
-			MaskedConv2D('B',2*channels, 2 * channels, kernel_size = 1, bias=False),
+			MaskedConv2D('B',2*self.channels, 2 * self.channels, kernel_size = 1, bias=False),
 			nn.ReLU(True),
-			MaskedConv2D('B',2* channels,2 * channels, kernel_size = 1, bias=False),
-			nn.Conv2d( 2 * channels, 256, 1)
+			MaskedConv2D('B',2* self.channels,2 * self.channels, kernel_size = 1, bias=False),
+			nn.Conv2d( 2 * self.channels, 256, 1)
 		)
 
 	def residual_block(self, x):
 		return (x + self.multiple_blocks(x))
 	
-	def forward(self,x):
+	def forward(self,x,**kwargs):
 		x = self.ConvA(x)
 		for i in range(self.nb_block):
-			x = residual_block(x)
+			x = self.residual_block(x)
 		x = self.end(x)
 		return x
 
